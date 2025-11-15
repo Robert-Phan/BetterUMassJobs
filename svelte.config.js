5ee0e1356e11f0695be810c4ec86cf1257e47846
Robert-Phan <fannk987@gmail.com>
2025-11-14 19:59:04 -0500

    Github pages first test deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : '/BetterUMassJobs'
+		}
+	}
 };
 
 export default config;
